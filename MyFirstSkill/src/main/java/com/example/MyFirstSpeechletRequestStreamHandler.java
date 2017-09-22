2c6dd83f7c008c1c5af789620a69433cbd882387
JH <gitlab.com@hendricksen.eu>
2017-09-22 15:48:33 +0200

    Add bash scripts for create and deploy

--- MyFirstSkill/src/main/java/com/example/MyFirstSpeechletRequestStreamHandler.java
+++ MyFirstSkill/src/main/java/com/example/MyFirstSpeechletRequestStreamHandler.java
@@ -6,12 +6,15 @@ import java.util.Set;
 import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 
 public final class MyFirstSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+
+    private static final Set<String> supportedApplicationIds = new HashSet<String>();
+
     // Be careful this makes your code available to anyone
     static {
         System.setProperty("com.amazon.speech.speechlet.servlet.disableRequestSignatureCheck", "true");
     }
 
     public MyFirstSpeechletRequestStreamHandler() {
-        super(new MyFirstSpeechlet(), new HashSet<String>());
+        super(new MyFirstSpeechlet(), supportedApplicationIds);
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # AlexaExamples
 
-Alexa examples using maven to help with automated deployment of the skill to AWS Lambda.
+Alexa examples using maven to help with automated deployment of the skill to AWS (Java based) Lambda.
 
 ## Credits
 
 This repository is based on the [AlexaExamples repository](https://github.com/johanjanssen/AlexaExamples) from Johan Janssen.
+Some inspiration has been taken from the [OneBusAway project](https://github.com/OneBusAway/onebusaway-alexa/blob/master/README.md)
