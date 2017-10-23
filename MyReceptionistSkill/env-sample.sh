963b6b9a9b0158c0ea4782c9a69bebbe001ea3c4
JH <gitlab.com@hendricksen.eu>
2017-10-23 11:51:42 +0200

    Improve readme to include all that has to be setup for a working skill with deployment via a single command.

---
+++ MyReceptionistSkill/env-sample.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+AWS_ACCESS_KEY="your-amazon-access-key-here"
+AWS_SECRET_KEY="your-secret-key-here"
+AWS_S3_BUCKET="your-simple-s3-bucketname-here"
+AWS_ROLE_ARN="your-role-here starts with arn:aws:iam::"
+AWS_REGION="us-east-1"
+
