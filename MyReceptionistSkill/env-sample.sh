95c78504ded7effb30298b2ae43e85e8724a30bd
JH <gitlab.com@hendricksen.eu>
2017-11-15 16:55:54 +0100

    Remove useless newline

--- MyReceptionistSkill/env-sample.sh
+++ MyReceptionistSkill/env-sample.sh
@@ -6,4 +6,3 @@ AWS_S3_BUCKET="your-simple-s3-bucketname-here"
 AWS_ROLE_ARN="your-role-here starts with arn:aws:iam::"
 AWS_REGION="us-east-1"
 AMAZON_SUPPORTED_APPLICATION_ID="amzn1.ask.skill.<your-id-here>"
-
